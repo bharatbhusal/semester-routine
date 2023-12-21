a117e6717d83b40e9a4558ca356320e912dce6f9
Bharat Bhusal <bharatbhusal78@gmail.com>
2023-12-21 09:39:21 +0530

    routine json

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Table } from "./Table"
+
+const App = () => {
+    return (
+        <div className='routine'>
+
+            <h1>Class Routine</h1>
+            <Table />
+        </div >
+    )
+}
+
+export default App
---
+++ src/Table.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import weekdays from "./routine.json"
+console.log(weekdays)
+
+export const Table = () => {
+    return (
+        <div>Table</div>
+    )
+}
