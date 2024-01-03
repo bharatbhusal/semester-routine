49fceee36a33a966f46006a45e51e59b347648b7
Bharat Bhusal <bharatbhusal78@gmail.com>
2024-01-03 17:05:24 +0530

    cool large

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const NavBar = ({ activeDay, handleDay }) => {
 
+    useEffect(() => {
+
+    })
     return (
         <nav>
             <div className={`day ${activeDay === 'Mon' ? 'active' : ''}`} onClick={handleDay}>
